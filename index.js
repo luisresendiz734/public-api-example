e30362feaf67aa32cee005ab9c118d1988d43d66
luisrpw0 <80994311+luisrpw0@users.noreply.github.com>
2021-04-01 13:50:30 -0600

    remove morgan --dev x2

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require("express");
-const morgan = require("morgan");
 const cors = require("cors");
 const { nanoid } = require("nanoid");
 const { Student } = require("./database");
 
 const app = express();
 app.use(express.json());
-//app.use(morgan("dev"));
+
 app.use(cors());
 
 const studentPath = "/students";
