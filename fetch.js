2e8deaea59a17d682db7a6f28fa1d3d3649f3d02
luisrpw0 <80994311+luisrpw0@users.noreply.github.com>
2021-04-01 14:45:40 -0600

    fetch operations added

---
+++ fetch.js
@@ -0,0 +1,71 @@
+// Base URL
+const URL = "https://escom-frontend-course.herokuapp.com/students"
+
+
+// GET
+const get = async () => {
+    try {
+        const response = await fetch(URL)
+        const data = await response.json()
+        console.log(data)
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+// POST
+const post = async () => {
+    const name = "Tony Stark"
+    const description = "I'm Iron-man!"
+    try {
+        const response = await fetch(URL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "Application/json"
+            },
+            body: JSON.stringify({ name, description })
+        })
+        const data = await response.json()
+        console.log(data)
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+// PUT
+const post = async () => {
+    const id = "<student-id>"
+    const name = "Tony Stark"
+    const description = "I'm Iron-man!"
+    try {
+        const response = await fetch(URL, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "Application/json"
+            },
+            body: JSON.stringify({ id, name, description })
+        })
+        const data = await response.json()
+        console.log(data)
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+// DELETE
+const post = async () => {
+    const id = "<student-id>"
+    try {
+        const response = await fetch(URL, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "Application/json"
+            },
+            body: JSON.stringify({ id })
+        })
+        const data = await response.json()
+        console.log(data)
+    } catch (error) {
+        console.log(error.message)
+    }
+}
